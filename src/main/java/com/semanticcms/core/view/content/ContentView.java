9dadbc4c828640c2cefe2c7f8917c60cc84e4972
AO Industries, Inc <support@aoindustries.com>
2021-03-14 11:56:27 -0500

    Separated a base "Any" layer so is more extensible.
    Working at the "Any" level means many type parameters, which can be "<?>".
    This would be used when supporting any different type of document.
    
    Working at the "Document" level means much fewer type parameters
    (pretty much only for current content model context), at the cost of
    being only the "Document" level.
    
    The ao-fluent-html-servlet project now has a parallel concrete implementation.
    This means it also has the single type parameter for current content model
    context.

--- src/main/java/com/semanticcms/core/view/content/ContentView.java
+++ src/main/java/com/semanticcms/core/view/content/ContentView.java
@@ -23,8 +23,7 @@
 package com.semanticcms.core.view.content;
 
 import com.aoindustries.collections.AoArrays;
-import com.aoindustries.html.FlowContent;
-import com.aoindustries.html.servlet.DocumentEE;
+import com.aoindustries.html.servlet.FlowContent;
 import com.aoindustries.servlet.http.Dispatcher;
 import com.aoindustries.web.resources.registry.Registry;
 import com.semanticcms.core.controller.PageUtils;
@@ -157,7 +156,7 @@ public class ContentView extends View {
 	}
 
 	@Override
-	public <__ extends FlowContent<DocumentEE, __>> void doView(ServletContext servletContext, HttpServletRequest request, HttpServletResponse response, __ flow, Page page) throws ServletException, IOException, SkipPageException {
+	public <__ extends FlowContent<__>> void doView(ServletContext servletContext, HttpServletRequest request, HttpServletResponse response, __ flow, Page page) throws ServletException, IOException, SkipPageException {
 		Dispatcher.include(
 			servletContext,
 			JSPX_TARGET,
